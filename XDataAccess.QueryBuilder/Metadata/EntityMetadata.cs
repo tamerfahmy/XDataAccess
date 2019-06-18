7cede8887bdc70aca55ed62bcab48836445cfae8
Fahmy <tamer.fahmy@evosoft.com>
2019-06-18 10:12:58 +0200

    Fix issue with Identity Attribute

--- XDataAccess.QueryBuilder/Metadata/EntityMetadata.cs
+++ XDataAccess.QueryBuilder/Metadata/EntityMetadata.cs
@@ -4,92 +4,77 @@ using System.Linq;
 using System.Reflection;
 using XDataAccess.QueryBuilder.Attributes;
 
-namespace XDataAccess.QueryBuilder.Metadata
-{
-    public sealed class EntityMetadata
-    {
+namespace XDataAccess.QueryBuilder.Metadata {
+    public sealed class EntityMetadata {
         public string EntityName { get; set; }
         public IEnumerable<AttributeMetadata> Attributes { get; set; }
 
-        public IEnumerable<AttributeMetadata> AttributesForInsertOrUpdate
-        {
-            get
-            {
-                return Attributes.Where(a => !a.Ignore && !a.Identity);
+        public IEnumerable<AttributeMetadata> AttributesForInsertOrUpdate {
+            get {
+                return Attributes.Where (a => !a.Ignore);
             }
         }
 
-        public IEnumerable<AttributeMetadata> IdentityAttributes
-        {
-            get
-            {
-                return Attributes.Where(a => a.Identity);
+        public IEnumerable<AttributeMetadata> IdentityAttributes {
+            get {
+                return Attributes.Where (a => a.Identity);
             }
         }
 
-        public bool HasIdentityAttribute { get { return IdentityAttributes.Count() > 0; } }
+        public bool HasIdentityAttribute { get { return IdentityAttributes.Count () > 0; } }
 
-        public bool IsValidForInsertOrUpdate { get { return AttributesForInsertOrUpdate.Count() > 0; } }
+        public bool IsValidForInsertOrUpdate { get { return AttributesForInsertOrUpdate.Count () > 0; } }
 
-        public AttributeMetadata GetAttribute(string propertyName)
-        {
-            return Attributes.FirstOrDefault(a => a.PropertyName == propertyName);
+        public AttributeMetadata GetAttribute (string propertyName) {
+            return Attributes.FirstOrDefault (a => a.PropertyName == propertyName);
         }
 
-        public static EntityMetadata Parse<TEntity>() where TEntity : class
-        {
-            var entityName = GetEntityName<TEntity>();
-            var attributes = GetAttributesMetadata<TEntity>();
+        public static EntityMetadata Parse<TEntity> () where TEntity : class {
+            var entityName = GetEntityName<TEntity> ();
+            var attributes = GetAttributesMetadata<TEntity> ();
 
-            return new EntityMetadata()
-            {
+            return new EntityMetadata () {
                 EntityName = entityName,
-                Attributes = attributes
+                    Attributes = attributes
             };
         }
 
-        public static EntityMetadata Parse<TEntity>(TEntity entity) where TEntity : class
-        {
-            var entityName = GetEntityName<TEntity>();
-            var attributes = GetAttributesMetadata<TEntity>(entity);
+        public static EntityMetadata Parse<TEntity> (TEntity entity) where TEntity : class {
+            var entityName = GetEntityName<TEntity> ();
+            var attributes = GetAttributesMetadata<TEntity> (entity);
 
-            return new EntityMetadata()
-            {
+            return new EntityMetadata () {
                 EntityName = entityName,
-                Attributes = attributes
+                    Attributes = attributes
             };
         }
 
-        private static IEnumerable<AttributeMetadata> GetAttributesMetadata<TEntity>(TEntity entity = null) where TEntity : class
-        {
-            var attributes = new List<AttributeMetadata>();
-            var props = typeof(TEntity).GetProperties();
+        private static IEnumerable<AttributeMetadata> GetAttributesMetadata<TEntity> (TEntity entity = null) where TEntity : class {
+            var attributes = new List<AttributeMetadata> ();
+            var props = typeof (TEntity).GetProperties ();
 
-            foreach (var property in props)
-            {
-                var attribute = new AttributeMetadata()
-                {
-                    Ignore = property.GetCustomAttribute(typeof(IgnoreAttribute)) != null,
-                    Identity = property.GetCustomAttribute(typeof(IdentityAttribute)) != null,
+            foreach (var property in props) {
+                var attribute = new AttributeMetadata () {
+                    Ignore = property.GetCustomAttribute (typeof (IgnoreAttribute)) != null,
+                    Identity = property.GetCustomAttribute (typeof (IdentityAttribute)) != null,
                 };
 
-                var propertyAttr = property.GetCustomAttribute(typeof(PropertyAttribute)) as PropertyAttribute;
+                var propertyAttr = property.GetCustomAttribute (typeof (PropertyAttribute)) as PropertyAttribute;
                 attribute.PropertyName = property.Name;
                 attribute.Name = propertyAttr?.Name ?? property.Name;
 
                 if (entity != null)
-                    attribute.Value = property.GetValue(entity);
+                    attribute.Value = property.GetValue (entity);
 
-                attributes.Add(attribute);
+                attributes.Add (attribute);
             }
 
             return attributes;
         }
 
-        private static string GetEntityName<TEntity>()
-        {
-            var entityType = typeof(TEntity);
-            var entityAttribute = entityType.GetCustomAttributes(typeof(EntityAttribute), false)?.FirstOrDefault();
+        private static string GetEntityName<TEntity> () {
+            var entityType = typeof (TEntity);
+            var entityAttribute = entityType.GetCustomAttributes (typeof (EntityAttribute), false)?.FirstOrDefault ();
 
             if (entityAttribute != null)
                 return (entityAttribute as EntityAttribute).Name;
